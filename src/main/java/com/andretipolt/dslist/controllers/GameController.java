87d95286b095759a239e0cee823191d0f8f13d21
AndreTipolt <andretipoltlopes@gmail.com>
2023-05-10 09:50:22 -0300

    Get Game By Id

--- src/main/java/com/andretipolt/dslist/controllers/GameController.java
+++ src/main/java/com/andretipolt/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.andretipolt.dslist.dto.GameDTO;
 import com.andretipolt.dslist.dto.GameMinDTO;
 import com.andretipolt.dslist.services.GameService;
 
@@ -21,5 +23,11 @@ public class GameController {
     public List<GameMinDTO> findAll(){
         List<GameMinDTO> listGameDTO = gameService.findAll();
         return listGameDTO;
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO gameDTO = gameService.findById(id);
+        return gameDTO;
     }
 }
--- src/main/java/com/andretipolt/dslist/services/GameService.java
+++ src/main/java/com/andretipolt/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.andretipolt.dslist.dto.GameDTO;
 import com.andretipolt.dslist.dto.GameMinDTO;
 import com.andretipolt.dslist.entities.Game;
 import com.andretipolt.dslist.repositories.GameRepository;
@@ -15,9 +17,16 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
 
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true) // Blocks database writting
+    public GameDTO findById(Long gameId){
+        Game game = gameRepository.findById(gameId).get(); // The ".get" is necessary because it's returns a Optional<Game>
+        return new GameDTO(game);
     }
 }
